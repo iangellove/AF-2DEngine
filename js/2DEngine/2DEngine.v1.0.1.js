45bddcc53f7125beeba39c70d07f46f0b563c22c
lijiaming <465973119@qq.com>
2020-12-10 18:39:09 +0800

    添加随机颜色

--- js/2DEngine/2DEngine.v1.0.1.js
+++ js/2DEngine/2DEngine.v1.0.1.js
@@ -655,6 +655,16 @@ Engine2D.util.showName = function(spirit){
 	spirit.context.fillText(spirit.name,spirit.x,spirit.y);
 }
 
+Engine2D.util.randomColor = function (){
+    var colorValue = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+    var colorArray = colorValue.split(",");
+    var color = "#";
+    for( var i = 0; i < 6; i++ ){
+        color += colorArray[ Math.floor( Math.random() * 16 ) ];
+    }
+    return color;
+}
+
 Engine2D.engine = {};
 
 Engine2D.engine.hitTest = function(scene){
